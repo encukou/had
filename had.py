ef4bd2d58e42ffee5b57686bfe7ac8bfd969ba46
Petr Viktorin <encukou@gmail.com>
2017-12-16 09:54:34 +0100

    Have two snakes

--- had.py
+++ had.py
@@ -1,17 +1,54 @@
 import random
 
-class Crash(Exception):
-    """The snake crashed"""
+class Snake():
+    def __init__(self, state):
+        self.state = state
+        self.alive = True
+        self.coords = [(0, 0)]
+        self.direction = 1, 0
+        self.queued_directions = []
+
+    def move(self):
+        if not self.alive:
+            return
+        if self.queued_directions:
+            old_x, old_y = self.direction
+            new_direction = self.queued_directions[0]
+            del self.queued_directions[0]
+            new_x, new_y = new_direction
+            if (old_x, old_y) != (-new_x, -new_y):
+                self.direction = new_direction
+        x, y = self.coords[-1]
+        dx, dy = self.direction
+        new_x = x + dx
+        new_y = y + dy
+        if new_x < 0:
+            new_x = self.state.width - 1
+        if new_y < 0:
+            new_y = self.state.height - 1
+        if new_x >= self.state.width:
+            new_x = 0
+        if new_y >= self.state.height:
+            new_y = 0
+        new_head = new_x, new_y
+        if any(new_head in snake.coords for snake in self.state.snakes):
+            self.alive = False
+            return
+        self.coords.append(new_head)
+        if new_head in self.state.fruit:
+            self.state.fruit.remove(new_head)
+            self.state.add_fruit()
+        else:
+            del self.coords[0]
 
 class State:
     def __init__(self):
         self.width = 10
         self.height = 10
-        self.snake = [(0, 0)]
         self.fruit = []
-        self.direction = 1, 0
         self.speed = 1/10
-        self.alive = True
+        self.snakes = [Snake(self) for i in range(2)]
+        self.snakes[1].coords = [(0, 3)]
 
     def __str__(self):
         rows = []
@@ -27,35 +64,15 @@ class State:
         return '\n'.join(' '.join(row) for row in reversed(rows))
 
     def move(self):
-        if not self.alive:
-            return
-        x, y = self.snake[-1]
-        dx, dy = self.direction
-        new_x = x + dx
-        new_y = y + dy
-        if new_x < 0:
-            new_x = self.width - 1
-        if new_y < 0:
-            new_y = self.height - 1
-        if new_x >= self.width:
-            new_x = 0
-        if new_y >= self.height:
-            new_y = 0
-        new_head = new_x, new_y
-        if new_head in self.snake:
-            self.alive = False
-        self.snake.append(new_head)
-        if new_head in self.fruit:
-            self.fruit.remove(new_head)
-            self.add_fruit()
-        else:
-            del self.snake[0]
+        for snake in self.snakes:
+            snake.move()
 
     def add_fruit(self):
         for i in range(100):
             x = random.randrange(self.width)
             y = random.randrange(self.height)
             position = x, y
-            if position not in self.snake and position not in self.fruit:
+            if not (any(position in s.coords for s in self.snakes) or
+                    position in self.fruit):
                 self.fruit.append(position)
                 return
--- ui.py
+++ ui.py
@@ -17,43 +17,43 @@ def draw():
     window.clear()
     tile_width = window.width / state.width
     tile_height = window.height / state.height
-    for x, y, in state.snake:
-        sprites.get_region(32, 0, 32, 32).blit(
-            x * tile_width, y * tile_height,
-            width=tile_width, height=tile_height)
+    for i, snake in enumerate(state.snakes):
+        for x, y, in snake.coords:
+            sprites.get_region(0, i*32, 32, 32).blit(
+                x * tile_width, y * tile_height,
+                width=tile_width, height=tile_height)
+        if not snake.alive:
+            x, y = snake.coords[-1]
+            sprites.get_region(32, 0, 32, 32).blit(
+                x * tile_width, y * tile_height,
+                width=tile_width, height=tile_height)
     for x, y, in state.fruit:
         sprites.get_region(32, 32, 32, 32).blit(
             x * tile_width, y * tile_height,
             width=tile_width, height=tile_height)
-    if not state.alive:
-        w = 64
-        h = 64
-        sprites.get_region(0, 0, 32, 32).blit(
-            (window.width-w)/2, (window.height-h)/2,
-            width=w, height=h)
 
 def tick(dt):
-    if not state.alive:
-        return
-    if queued_directions:
-        old_x, old_y = state.direction
-        new_direction = queued_directions[0]
-        del queued_directions[0]
-        new_x, new_y = new_direction
-        if (old_x, old_y) != (-new_x, -new_y):
-            state.direction = new_direction
     state.move()
     pyglet.clock.schedule_once(tick, state.speed*1.5 - dt/5)
 
 def key_press(symbol, mod):
-    if symbol in (pyglet.window.key.A, pyglet.window.key.LEFT):
-        queued_directions.append((-1, 0))
-    elif symbol in (pyglet.window.key.D, pyglet.window.key.RIGHT):
-        queued_directions.append((1, 0))
-    elif symbol in (pyglet.window.key.S, pyglet.window.key.DOWN):
-        queued_directions.append((0, -1))
-    elif symbol in (pyglet.window.key.W, pyglet.window.key.UP):
-        queued_directions.append((0, 1))
+    snakes = state.snakes
+    if symbol == pyglet.window.key.A:
+        snakes[0].queued_directions.append((-1, 0))
+    if symbol == pyglet.window.key.LEFT:
+        snakes[1].queued_directions.append((-1, 0))
+    if symbol == pyglet.window.key.D:
+        snakes[0].queued_directions.append((1, 0))
+    if symbol == pyglet.window.key.RIGHT:
+        snakes[1].queued_directions.append((1, 0))
+    if symbol == pyglet.window.key.S:
+        snakes[0].queued_directions.append((0, -1))
+    if symbol == pyglet.window.key.DOWN:
+        snakes[1].queued_directions.append((0, -1))
+    if symbol == pyglet.window.key.W:
+        snakes[0].queued_directions.append((0, 1))
+    if symbol == pyglet.window.key.UP:
+        snakes[1].queued_directions.append((0, 1))
 
 window.push_handlers(
     on_draw=draw,
